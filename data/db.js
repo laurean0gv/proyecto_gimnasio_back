6bf493976ce0d925cfdcb468a620c98a4e5c85b2
laurean0gv <105830938+laurean0gv@users.noreply.github.com>
2024-06-24 01:24:27 -0300

    Update db.js

--- data/db.js
+++ data/db.js
@@ -5,10 +5,10 @@ config();
 
 
 
-const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
-    host: process.env.DB_HOST,
+const db = new Sequelize("railway","root","ExjEyUiGJhYIcWoBJAncGSdhqXNMTLDH",{
+    host: "monorail.proxy.rlwy.net",
     dialect: "mysql",
-    port: process.env.PORT
+    port: 43201
 });
 
     module.exports=db;
