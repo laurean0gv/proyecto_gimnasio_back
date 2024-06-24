f992c003a3bf3930b13a735d18b1773e9c71eb0c
laurean0gv <laureanogv@gmail.com>
2024-06-24 02:08:18 -0300

    probando listen para subir al servidor

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require ("express")
 const app = express()
+<<<<<<< HEAD
 const port=3000
+=======
+const port = process.env.PORT || 3000;
+
+
+>>>>>>> 35bea2c (probando listen para subir al servidor)
 const cors = require ("cors")
 const storeRouter = require ("./routes/storeRouter.js")
 const db = require ("./data/db.js")
@@ -18,14 +24,14 @@ app.use ("/store", storeRouter)
 const conexionDb = async () => {
     try {
         await db.authenticate();
-        console.log(db.config);
+        console.log(db.config)
         console.log("conexion db Ok");
     } catch (error) {
         console.log(error);
     }
 }
-
-app.listen(port, ()=>{
+// Listen on `port` and 0.0.0.0
+app.listen(port, "0.0.0.0", function () {
     conexionDb()
     console.log(`Server ok en el puerto ${port}`)
 }) 
