f7f7bb2ee9dcfcc090a96740286155bc2e3bb36c
laurean0gv <laureanogv@gmail.com>
2024-06-24 02:51:48 -0300

    ajustes sobre db para hacer funcionar el servidor

--- data/db.js
+++ data/db.js
@@ -16,7 +16,7 @@ const db = new Sequelize(process.env.MYSQLDATABASE,process.env.MYSQLUSER,process
     dialect: "mysql",
     port: process.env.MYSQLPORT
 })
-
+console.log("Vamos a ver la configuracion LPM "+db.config);
 
 /*
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
