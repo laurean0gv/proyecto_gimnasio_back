da940167ec5da587c6d6955f10c3f3a6eb688312
laurean0gv <laureanogv@gmail.com>
2024-06-24 02:23:19 -0300

    ajustes finales para dejarlo deployado en el servidor

--- data/db.js
+++ data/db.js
@@ -13,7 +13,7 @@ const db = new Sequelize("railway","root","ExjEyUiGJhYIcWoBJAncGSdhqXNMTLDH",{
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
     host: process.env.DB_HOST,
     dialect: "mysql",
-    port: process.env.PORT
+    port: process.env.DB_PORT
 })
 
     module.exports=db;
