4f813d1181adc3eec6b308d10631feec9ae71e20
laurean0gv <laureanogv@gmail.com>
2024-06-24 04:24:08 -0300

    ajustes finales para hacer funcionar el servidor

--- data/db.js
+++ data/db.js
@@ -1,16 +1,6 @@
 const {Sequelize} = require ("sequelize")
-const express = require ("express")
 const { config } = require ('dotenv');
 config();
-
-/*
-const db = new Sequelize("railway","root","ExjEyUiGJhYIcWoBJAncGSdhqXNMTLDH",{
-    host: "monorail.proxy.rlwy.net",
-    dialect: "mysql",
-    port: 43201
-});*/
-console.log(process.env)
-
 
 /*
 const db = new Sequelize(process.env.MYSQLDATABASE,process.env.MYSQLUSER,process.env.MYSQLPASSWORD,{
@@ -22,9 +12,9 @@ console.log(`Vamos a ver la configuracion LPM ${db.config.database} - ${db.confi
 
 */
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
-    host: "monorail.proxy.rlwy.net",
+    host: process.env.DB_HOST,
     dialect: "mysql",
-    port: 43201
+    port: process.env.DB_PORT
 })
 
     module.exports=db;
