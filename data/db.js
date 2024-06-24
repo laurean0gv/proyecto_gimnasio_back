9e67faeb50af7130f17f6a7815b62fa066be71dd
laurean0gv <laureanogv@gmail.com>
2024-06-24 02:47:46 -0300

    ajustes sobre db para hacer funcionar el servidor

--- data/db.js
+++ data/db.js
@@ -3,11 +3,21 @@ const {Sequelize} = require ("sequelize")
 const { config } = require ('dotenv');
 config();
 
+/*
 const db = new Sequelize("railway","root","ExjEyUiGJhYIcWoBJAncGSdhqXNMTLDH",{
     host: "monorail.proxy.rlwy.net",
     dialect: "mysql",
     port: 43201
-});
+});*/
+
+
+const db = new Sequelize(process.env.MYSQLDATABASE,process.env.MYSQLUSER,process.env.MYSQLUSER,{
+    host: process.env.MYSQLHOST,
+    dialect: "mysql",
+    port: process.env.MYSQLPORT
+})
+
+
 /*
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
     host: process.env.DB_HOST,
