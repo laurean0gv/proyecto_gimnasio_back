8852624af926f408b4dae03b1e23c069d2aa5472
laurean0gv <laureanogv@gmail.com>
2024-06-27 15:15:59 -0300

    agregamos store.js

--- pages/store.js
+++ pages/store.js
@@ -1,27 +1,41 @@
-const axios = require ("axios")
+/* const axios = require ("axios")*/
 
 //ejecuto el llamado a la funcion con la carga de la web 
-window.addEventListener("load",()=>{    
+window.addEventListener("load", () => {
     let titulo = document.getElementById("titulo").textContent;
     console.log(titulo);
-    if(titulo=="Store"){
+    if (titulo == "Store") {
         mostrarProductos();
     }
 });
 
 
+//funcion de carga y e impesion de productos
+mostrarProductos = async () => {
 
-
-getNameAxios = async (idProducto) => {
     try {
-        const resPost = await axios (`https://proyectogimnasioback-production.up.railway.app/store/`)
-        const resUser = await axios(`https://proyectogimnasioback-production.up.railway.app/store/${idProducto.data.id}`)
-        console.log(resUser.data.name);
-        
+        const resPost = await axios(`http://localhost:3000/store/`)
+        /* const producto = await axios(`http://localhost:3000/store/${idProducto}`) */
+        let productos="";
+        resPost.data.forEach(agregaProducto)
+
+        function agregaProducto (producto){
+            productos+=`<article class="producto">
+            <a href="${producto.foto}">
+                <div class="cont-img"><img src="${producto.foto}" alt="${producto.titulo}"></div>
+                <hr>
+                    <h3>${producto.descripcion}</h3>
+                    <p class="precio">$${producto.precio}</p>
+            </a>
+        </article>`
+        }
+        document.querySelector(".productos").innerHTML = productos
+
+
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
 }
 
-getNameAxios(10);
+
 
